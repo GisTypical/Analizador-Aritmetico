958657f3fb7c5edc0b5893957d5e9038e0a2cd1a
Gerardo <gerardopapaleco@gmail.com>
2021-10-24 20:35:48 -0400

    initial things

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string expression;
+
+  cout << "Ingrese la expresión: ";
+
+  getline(cin, expression);
+
+  cout << "Hacer algo con: " << expression;
+
+  return 0;
+}
